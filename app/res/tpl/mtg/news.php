4199b397dff6ce02619087c91775b0f41dbfbd6c
Stephan <sh@fettundrosig.com>
2013-07-05 10:59:25 +0200

    Added Google Font, fixed a I18n issue and faked news

---
+++ app/res/tpl/mtg/news.php
@@ -0,0 +1,10 @@
+<div class="sidebar news">
+	<h2><?php echo I18n::__('mtg_news_h1') ?></h2>
+<?php foreach ($news as $_news_id => $_news): ?>
+	<div class="news item">
+		<h3>Hendis in endigenis dorlerunto</h3>
+		<p>Lorem borem, sorem, lirumlarum, Löffelstielum. Pilum bringum umo humanes est. Esse umbro, bro ba ba.</p>
+		<a href="#readmore">Preferatir readtor</a>
+	</div>
+<?php endforeach ?>
+</div>
