b23de0d6bf21cc99e803fc6874ce3cf389a60f41
Stephan <sh@fettundrosig.com>
2013-08-23 13:10:16 +0200

    News do not take language in account

--- app/res/tpl/mtg/news.php
+++ app/res/tpl/mtg/news.php
@@ -1,4 +1,4 @@
-<?php $_news = R::find('news', 'language = ? AND newscat_id = ? AND archived = ? AND online = 1 ORDER BY pubdatetime DESC', array(Flight::get('language'), 1, 0)) ?>
+<?php $_news = R::find('news', 'language = ? AND newscat_id = ? AND archived = ? AND online = 1 ORDER BY pubdatetime DESC', array('de', 1, 0)) ?>
 <div class="sidebar news">
 	<h2><?php echo I18n::__('mtg_news_h1') ?></h2>
 	<?php if (empty($_news)): ?>
@@ -6,9 +6,13 @@
 	<?php else: ?>
 	    <?php foreach ($_news as $_news_id => $_news): ?>
 	    <div class="news item">
-	        <h3><?php echo htmlspecialchars($_news->name) ?></h3>
+			<?php if ($_news->name): ?>
+			<h3><?php echo htmlspecialchars($_news->name) ?></h3>	
+			<?php endif ?>
 	        <?php echo Flight::textile($_news->teaser) ?>
+			<?php if ($_news->content): ?>
 			<a href="<?php echo Url::build('/news/%d', array($_news->getId())) ?>"><?php echo I18n::__('mtg_read_news') ?></a>
+			<?php endif ?>
 	    </div>
 	    <?php endforeach ?>
 	<?php endif ?>
