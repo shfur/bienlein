9064e84013d8018daa62545e7367b23dc009c5a3
Stephan <sh@fettundrosig.com>
2013-09-04 12:46:27 +0200

    Check if file exists in image module frontend

--- app/res/tpl/module/image/frontend.php
+++ app/res/tpl/module/image/frontend.php
@@ -8,6 +8,7 @@
  * @version $Id$
  */
 ?>
+<?php if ( ! file_exists(Flight::get('upload_dir').'/'.$record->content)) return ?>
 <?php list($width, $height, $type, $attr) = getimagesize(Flight::get('upload_dir').'/'.$record->content); ?>
 <img
     src="<?php echo Flight::get('media_path').'/'.$record->content ?>"
